85233670d54c6b252b682d95cb0fbe4d26af2d1a
Samrat Bhandari <samratbhandari2000@gmail.com>
2022-09-14 16:56:22 +0545

    Processing

--- Program.cs
+++ Program.cs
@@ -9,3 +9,25 @@ Console.WriteLine("1 For Addition");
 Console.WriteLine("2 For Substraction");
 Console.WriteLine("3 For Muliplication");
 Console.WriteLine("4 For Division");
+action = int.Parse(Console.ReadLine());
+int Addition(int number1, int number2)
+{
+    int result = number1 + number2;
+    return result;
+}
+int Substraction(int number1, int number2)
+{
+    int result = number1 - number2;
+    return result;
+}
+int Multiplication(int number1, int number2)
+{
+    int result = number1 * number2;
+    return result;
+}
+int Division(int number1, int number2)
+{
+    int result = number1 / number2;
+    return result;
+}
+Console.WriteLine("The result is {0}", result);
